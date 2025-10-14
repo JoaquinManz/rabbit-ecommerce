79e40986f28efe6f782ce141adf0cdc552b5a387
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-10-14 20:31:16 -0300

    added delete and update routes for orders in the admin dashboard. Completed the backend!

--- backend/routes/adminOrderRoutes.js
+++ backend/routes/adminOrderRoutes.js
@@ -17,4 +17,47 @@ router.get('/', protect, admin, async (req, res) => {
     }
 });
 
+
+// @route PUT /api/admin/orders/:id
+// @desc Update order status(admin only)
+// @access Private/Admin
+router.put("/:id", protect, admin, async (req, res) => {
+    try {
+       const order = await Order.findById(req.params.id);
+       
+       if(order) {
+           order.status = req.body.status || order.status;
+           order.isDelivered = req.body.status === "Delivered" ? true : order.isDelivered;
+           order.deliveredAt = req.body.status === "Delivered" ? Date.now() : order.deliveredAt;
+
+           const updatedOrder = await order.save();
+           res.json(updatedOrder);
+       } else {
+           res.status(404).json({ message: "Order not found" });
+       }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+// @route DELETE /api/admin/orders/:id
+// @desc Delete order (admin only)
+// @access Private/Admin
+router.delete("/:id", protect, admin, async(req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+
+        if(order) {
+            await order.deleteOne();
+            res.json({ message: "Order removed successfully" });
+        } else {
+            res.status(404).json({ message: "Order not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+})
+
 module.exports = router;
