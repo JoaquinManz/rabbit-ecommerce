de7d84892c02a0d97a41dca9841ccc8370aac02b
Joaquin Manzanares <altairyezio2087@gmail.com>
2025-10-15 22:14:39 -0300

    slices for admin products and orders

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from"./slices/productsSlice";
 import cartReducer from "./slices/cartSlice";
-import checkoutReducer from "./slices/checkoutSlice"
-import orderReducer from "./slices/orderSlice"
-import adminReducer from "./slices/adminSlice"
+import checkoutReducer from "./slices/checkoutSlice";
+import orderReducer from "./slices/orderSlice";
+import adminReducer from "./slices/adminSlice";
+import adminProductReducer from "./slices/adminProductSlice";
+import adminOrderReducer from "./slices/adminOrderSlice";
 
 const store = configureStore({
     reducer: {
@@ -14,7 +16,9 @@ const store = configureStore({
         checkout: checkoutReducer,
         orders: orderReducer,
         admin: adminReducer,
+        adminProduct: adminProductReducer,
+        adminOrders: adminOrderReducer,
     },
-})
+});
 
 export default store;
